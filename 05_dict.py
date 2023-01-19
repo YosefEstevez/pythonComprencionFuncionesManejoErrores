2c401f7ea89d6304351ccdd435f98326abafeebc
Yosef <parcincinternacional@gmail.com>
2023-01-19 17:05:15 -0300

    Dictionary Comprehension

---
+++ 05_dict.py
@@ -0,0 +1,31 @@
+'''
+dict = {}
+for i in range(1, 5):
+  dict[i] = i * 2
+
+print(dict)
+
+dict_v2 = { i: i * 2 for i in range(1, 5)}
+print(dict_v2)
+
+#random.randint =numero random entero
+import random
+countries = ['col', 'mex', 'bol', 'pe']
+population = {}
+for country in countries:
+  population[country] = random.randint(1, 100)
+
+print(population)
+
+population_v2 = { country: random.randint(1, 100)  for country in countries}
+print(population_v2)
+'''
+names = ['yosef', 'angelica', 'rico']
+ages = [12, 56, 98]
+
+#creacion de diccionario a partir de listas
+#zip = union entre listas pero debe preceder list y se convierte en tupla
+print(list(zip(names, ages)))
+
+new_dict = {name: age for (name, age) in zip(names, ages)}
+print(new_dict)
