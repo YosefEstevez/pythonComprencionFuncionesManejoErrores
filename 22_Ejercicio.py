8fa1096965c132c944061f82d83e2925fa3921de
Yosef <parcincinternacional@gmail.com>
2023-04-09 18:30:27 -0300

    Ejercicio de Try

---
+++ 22_Ejercicio.py
@@ -0,0 +1,15 @@
+#Captura la excepción: ZeroDivisionError
+
+def my_divide(a, b):
+   # Escribe tu solución 👇
+    try:
+     result = a / b
+    Except ZeroDivisionError as error:
+     result = 'No se puede dividir por 0'
+    return result
+    
+response = my_divide(10, 2)
+print(response) # 5
+
+response = my_divide(10, 0)
+print(response) # No se puede dividir por 0
